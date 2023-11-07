e5e94292aaefa728f4dac7dc7684bc08324eebfb
Rachael Momber <rlmomber@gmail.com>
2023-11-07 18:01:51 +0900

    finish rails-wikinimous, no refactor or bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Display all articles
+  get 'articles', to: 'articles#index'
+  # Create a new article
+  # New must be before Show
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+  # Show specific article
+  get 'articles/:id', to: 'articles#show', as: :article
+  # Edit a task
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  # Delete a task
+  delete 'articles/:id', to: 'articles#destroy'
 end
